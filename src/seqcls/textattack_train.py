d7ad713116fe4392e15dc3c3993a5e3527288248
matglima-utaustin <157311928+matglima-utaustin@users.noreply.github.com>
2024-12-02 14:40:44 -0300

    Update textattack_train.py

--- src/seqcls/textattack_train.py
+++ src/seqcls/textattack_train.py
@@ -30,7 +30,7 @@ def preprocess_function(examples):
 
         result = tokenizer(*args, padding=True, max_length=512, truncation=True)
         # Create label map
-        labels = sorted(set(item[label_key].lower() for item in examples if label_key in item))
+        labels = sorted(set(item[3].lower() for item in examples if label_key in item))
         label_map = {label: i for i, label in enumerate(labels)}
         result["label"] = label_map[item[label_key].lower()]
         return result
